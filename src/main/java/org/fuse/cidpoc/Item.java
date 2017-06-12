a98611ec63f35fec1ee33e79bbcd5e2940b9199c
Thomas Diesler <thomas.diesler@jboss.com>
2017-06-12 16:11:31 +0200

    [resolves #1] Move to java based caps/reqs

--- src/main/java/org/fuse/cidpoc/Item.java
+++ src/main/java/org/fuse/cidpoc/Item.java
@@ -1,5 +1,10 @@
 package org.fuse.cidpoc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -10,15 +15,9 @@ public abstract class Item {
         private final String name;
         private final int value;
 
-        Capability(String name, int value) {
+        public Capability(String name, int value) {
             this.name = name;
             this.value = value;
-        }
-
-        static Capability parse(String spec) {
-            String name = spec.substring(0, 1);
-            String number = spec.substring(1);
-            return new Capability(name, Integer.parseInt(number));
         }
 
         public String getName() {
@@ -39,23 +38,10 @@ public abstract class Item {
         private final int min;
         private final int max;
 
-        Requirement(String name, int min, int max) {
+        public Requirement(String name, int min, int max) {
             this.name = name;
             this.min = min;
             this.max = max;
-        }
-
-        static Requirement parse(String spec) {
-            String name = spec.substring(0, spec.indexOf('('));
-            String min = spec.substring(spec.indexOf('(') + 1, spec.lastIndexOf(')'));
-            String max;
-            if (min.indexOf('-') > 0) {
-                max = min.substring(min.indexOf('-') + 1);
-                min = min.substring(0, min.indexOf('-'));
-            } else {
-                max = min;
-            }
-            return new Requirement(name, Integer.parseInt(min), Integer.parseInt(max));
         }
 
         public String getName() {
@@ -79,18 +65,16 @@ public abstract class Item {
         }
     }
 
+    public String getVName() {
+        return getClass().getSimpleName() + "-" + getVersion(getClass());
+    }
+
     public abstract List<Item> getDependencies();
 
-    public String getVName() {
-        return Utils.getVName(getClass());
-    }
-
-    public Capability getCapability() {
-        return Utils.getCapability(getClass());
-    }
+    public abstract Capability getCapability();
 
     public List<Requirement> getRequirements() {
-        return Utils.getRequirements(getClass());
+        return Collections.emptyList();
     }
 
     public boolean isSatisfied() {
@@ -118,6 +102,14 @@ public abstract class Item {
 
     public void transitiveStatus() {
         transitiveStatus(new HashSet<>());
+    }
+    
+    public static String getVersion(Class<?> clazz) {
+        try (InputStream ins = clazz.getResourceAsStream("version")) {
+            return new BufferedReader(new InputStreamReader(ins)).readLine();
+        } catch (IOException ex) {
+            throw new IllegalStateException(ex);
+        }
     }
     
     private void transitiveStatus(Set<String> visited) {
