34b1c536ad54829315ebf2ff4469d42282786b56
Thomas Diesler <thomas.diesler@jboss.com>
2017-06-23 09:04:57 +0200

    A4

--- src/main/java/org/fuse/cidpoc/a/A.java
+++ src/main/java/org/fuse/cidpoc/a/A.java
@@ -9,7 +9,7 @@ public class A extends Item {
 
     @Override
     public Capability getCapability() {
-        return new Capability("A", 3);
+        return new Capability("A", 4);
     }
 
     @Override
